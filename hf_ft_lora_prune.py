263cb4a339c78582a9bdbef0a7a2af49f7039b3c
Joey Bejjani <jpbejjani@hotmail.com>
2024-11-15 16:41:18 -0500

    report model sparsity during ft with progressive pruning

--- hf_ft_lora_prune.py
+++ hf_ft_lora_prune.py
@@ -1,4 +1,4 @@
-# Progressiely prunes `model_name` while fine tuning on `dataset` with LoRA
+# Progressiely prunes `model_name` while fine tuning on `dataset` with rsLoRA
 # Saves checkpoint to `output_dir`
 
 
@@ -30,40 +30,38 @@ def compute_metrics(eval_preds):
     return metric.compute(predictions=predictions, references=labels)
 
 
-def compute_global_sparsity(model):
-    total_params = 0
-    zero_params = 0
-
-    for param in model.parameters():
-        # Flatten the tensor to count zeros and total elements
-        total_params += param.numel()
-        zero_params += torch.sum(param == 0).item()
-    
-    global_sparsity = zero_params / total_params
-    return global_sparsity
-
-
 class PruningCallback(TrainerCallback):
     def __init__(self, model, ptg=0.1):
-        self.ptg = ptg
-        self.method = prune.L1Unstructured
         self.model = model
-        
-        # Get the frozen pre-trained, non-LoRA parameters
-        self.params = []
+        self.ptg = ptg  # percentage of params to prune
+        self.method = prune.L1Unstructured  # pruning method
+        self.params = []  # params to prune, list of tuples: (module, param_name)
 
-        # Iterate through all named modules in the model
+        # Iterate through all modules in the model
+        # and get the frozen / non-LoRA parameters to be pruned
+        # skip embedding layers to mitigate accuracy degradation
         for name, module in model.named_modules():
-            if hasattr(module, 'weight') and 'weight' in module._parameters:  # Ensure module has a weight attribute and parameter
-                if not module.weight.requires_grad and not isinstance(module, nn.Embedding):  # Filter for non-embedding and non-trainable parameters
+            # Ensure module has a weight attribute and parameter
+            if hasattr(module, 'weight') and 'weight' in module._parameters:
+                # Filter for non-embedding and frozen / non-trainable parameters
+                if not module.weight.requires_grad and not isinstance(module, nn.Embedding):
                     assert("lora" not in name)
                     self.params.append((module, 'weight'))
 
-    # Callback function: called at the end of each training epoch
+        # Count total number of params and number of LoRA params for sparsity reports
+        self.total_params = 0
+        self.lora_params = 0
+
+        for name, param in model.named_parameters():
+            if "lora" in name:
+                self.lora_params += param.numel()
+            self.total_params += param.numel()
+
+    # Function called at the end of each training epoch
     def on_epoch_end(self, args, state, control, **kwargs):
-        print(f"\nPruning {self.ptg}% of pretrained weights after epoch {state.epoch}")
+        print(f"\nPruning {self.ptg * 100:.2f}% of pretrained weights after epoch {state.epoch}")
         self.prune_pretrained()
-        self.compute_global_sparsity()
+        self.report_sparsity()
 
     # Applies pruning `weight_mask` to pretrained, non-LoRA model parameters
     # Retains previous mask, allowing cumulative pruning
@@ -73,18 +71,21 @@ class PruningCallback(TrainerCallback):
             pruning_method=self.method,
             amount=self.ptg,
         )
-            
-    def compute_global_sparsity(self):
-        print(
-            "sparsity: {:.2f}%".format(
-                100. * float(
-                    torch.sum(self.model.distilbert.transformer.layer[0].attention.q_lin.weight == 0)
-                )
-                / float(
-                    self.model.distilbert.transformer.layer[0].attention.q_lin.weight.nelement()
-                )
-            )
-        )
+        
+    # Reports sparsity of frozen / pretrained weights, as well as overall model sparsity
+    def report_sparsity(self):
+        zero_params = 0
+        
+        # Iterate over all parameters in the model
+        for name, module in self.model.named_modules():
+            if hasattr(module, 'weight'):
+                zero_params += torch.sum(module.weight == 0).item()
+        
+        sparsity_frozen = (zero_params / (self.total_params - self.lora_params)) * 100 if self.total_params > 0 else 0
+        sparsity_all = (zero_params / self.total_params) * 100 if self.total_params > 0 else 0
+
+        print(f"Pretrained Weight Sparsity: {sparsity_frozen:.2f}%")
+        print(f"Overall Model Sparsity (including LoRA): {sparsity_all:.2f}%")
         
     # Removes the pruning reparameterization to make pruning permanent
     # Necessary in order to consolidate pruning changes permanently or export the model
@@ -103,11 +104,12 @@ if __name__ == "__main__":
 
     model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=5)
 
-    # Target LoRA modules - the attention and MLP layers
+    # A LoRA adapter will be attached to each target module
+    # typically the attention and MLP layers of a transformer
     target_modules = []
-    # Iterate over the layers of the transformer to find LoRA adapter target modules
     num_layers = len(model.distilbert.transformer.layer)
     target = "distilbert.transformer.layer"
+    # Iterate over the transformer layers to get target modules
     for i in range(num_layers):
         target_modules.extend([
             f"{target}.{i}.attention.q_lin",    # Query projection in attention
@@ -143,7 +145,7 @@ if __name__ == "__main__":
     )
     
     # Create callback
-    pruning_callback = PruningCallback(model, ptg=0.2)
+    pruning_callback = PruningCallback(model, ptg=0.05)
 
     trainer = Trainer(
         model=model,
