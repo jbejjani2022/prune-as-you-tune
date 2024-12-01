79001b7842622828e770a97ea4f9cd6cfbe2f5f9
sgGoel <ms.swati.goel@gmail.com>
2024-11-30 21:02:21 -0500

    support for smaller train/test sets

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ lora_config = LoraConfig(
 evaluator = BertBaseFineTuneEvaluator(
     dataset=dataset,
     training_args=training_args,
-    max_length=5000,  # set max_length = None if you don't want to truncate samples
+    max_length=None,  # set max_length = None if you don't want to truncate samples
     lora_config=lora_config,
     pruning_method=pruning_method,
     sparsity_target=sparsity_target,
@@ -54,7 +54,8 @@ evaluator = BertBaseFineTuneEvaluator(
     temp=2,
     device=device,
     save_dir=save_dir,
-    eval_ppl=True  # evaluate perplexity on orig task after each finetuning
+    eval_ppl=True,  # evaluate perplexity on orig task after each finetuning
+    num_samples=5000 # set num_samples = None if we want to run with all samples
 )
 
 #evaluator.prune_lora_finetune()
