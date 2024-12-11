59087e69d4fa086b63e3a4360ee4cb61bbf36b97
Amulya Garimella <amulyagarimella@users.noreply.github.com>
2024-12-10 20:29:59 -0500

    move inputs to cuda

--- src/dataset.py
+++ src/dataset.py
@@ -49,6 +49,7 @@ class FineTuneDataset:
         texts = self.orig_dataset["text"]
         #print(texts)
         inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+        inputs.to(self.orig_model.device)
         with torch.inference_mode():
             outputs = self.orig_model(**inputs)
         #print(outputs)
